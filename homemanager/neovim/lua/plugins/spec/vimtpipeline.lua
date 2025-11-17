c42a654df54f46b40fb8da5b574c42ab3cf5d867
quirinecker <quirin.ecker@bajupa.com>
2025-11-17 20:13:29 +0100

    using bash for tpipeline in the background. fish is slow

--- homemanager/neovim/lua/plugins/spec/vimtpipeline.lua
+++ homemanager/neovim/lua/plugins/spec/vimtpipeline.lua
@@ -1,6 +1,7 @@
 return {
 	'vimpostor/vim-tpipeline',
 	config = function()
+		vim.opt.shell = vim.fn.exepath("bash")
 		vim.g.tpipeline_autoembed = 1
 		vim.g.tpipeline_restore = 1
 		vim.g.tpipeline_clearstl = 1
