c218e789454cad38e42003ec0ebaf874bdd94648
quirinecker <quirin.ecker@bajupa.com>
2025-12-06 21:17:47 +0100

    fixed #6

--- homemanager/neovim/lua/plugins/spec/oil.lua
+++ homemanager/neovim/lua/plugins/spec/oil.lua
@@ -1,20 +1,20 @@
 local function oil()
-    require("oil").open()
+	require("oil").open()
 end
 
 return {
-    'stevearc/oil.nvim',
-    opts = {
-        view_options = {
-            show_hidden = true
-        }
-    },
-    lazy = false,
-    cmd = 'Oil',
-    keys = {
-        {"<leader>ft", oil, ""}
-    },
-    dependencies = {
-        'kyazdani42/nvim-web-devicons',
-    }
+	'stevearc/oil.nvim',
+	opts = {
+		view_options = {
+			show_hidden = true
+		}
+	},
+	lazy = false,
+	cmd = 'Oil',
+	keys = {
+		{ "-", oil, "" }
+	},
+	dependencies = {
+		'kyazdani42/nvim-web-devicons',
+	}
 }
