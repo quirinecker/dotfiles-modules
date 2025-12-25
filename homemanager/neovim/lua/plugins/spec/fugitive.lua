17a1e0c788fa3c28c7336198c4962e1834c48d3d
quirinecker <quirin.ecker@bajupa.com>
2025-12-25 09:02:27 +0100

    fugitive things are opening in a new tab now. At least the 2 most important ones.

--- homemanager/neovim/lua/plugins/spec/fugitive.lua
+++ homemanager/neovim/lua/plugins/spec/fugitive.lua
@@ -1,12 +1,12 @@
 return {
 	'tpope/vim-fugitive',
 	keys = {
-		{ '<leader>gg', ':Git<cr>', 'Git Status' },
+		{ '<leader>gg', ':tab Git<cr>', 'Git Status' },
 		{ '<leader>gc', function()
 			if vim.bo.filetype == 'fugitive' then
 				vim.cmd.close()
 			end
-			vim.cmd("Git commit")
+			vim.cmd("tab Git commit")
 		end, '(g)it (c)ommit' }
 	},
 	cmd = {
