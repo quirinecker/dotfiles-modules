d8126bd7f5ceb2fe1373193461f6b1694ddf7007
quirinecker <quirin.ecker@bajupa.com>
2026-04-04 13:14:07 +0200

    added kde connect to noctalia. its still work in progress though

--- homemanager/noctalia.nix
+++ homemanager/noctalia.nix
@@ -10,9 +10,27 @@
   };
 
   config = lib.mkIf config.modules.noctalia.enable {
+    services.kdeconnect.enable = true;
+
     programs.noctalia-shell.enable = true;
     programs.noctalia-shell.settings =
       (builtins.fromJSON (builtins.readFile ./noctalia/config.json)).settings;
+    programs.noctalia-shell.plugins = {
+      sources = [
+        {
+          enabled = true;
+          name = "Official Noctalia Plugins";
+          url = "https://github.com/noctalia-dev/noctalia-plugins";
+        }
+      ];
+      states = {
+        kde-connect = {
+          enabled = true;
+          sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins/";
+        };
+      };
+      version = 2;
+    };
     home.file."${config.home.homeDirectory}/Pictures/Wallpapers".source = ./backgrounds;
   };
 
