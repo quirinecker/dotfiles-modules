396155c4e9db7bbe655a00a6ecc943761f732016
quirinecker <quirin.ecker@bajupa.com>
2026-05-18 16:20:57 +0200

    hardware config can now be set individually in the host specific configurations

--- homemanager/hyprland.nix
+++ homemanager/hyprland.nix
@@ -10,6 +10,11 @@ in
 {
   options = {
     modules.hyprland.enable = lib.mkEnableOption "hyprland";
+    modules.hyprland.hardwareConfig = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      description = "extra configuration that is hardware specific";
+    };
   };
 
   config = lib.mkIf config.modules.hyprland.enable {
@@ -21,6 +26,8 @@ in
       "hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink ./hyprland/hyprland.conf;
       "hypr/hypridle.conf".source = config.lib.file.mkOutOfStoreSymlink ./hyprland/hypridle.conf;
       "backgrounds".source = config.lib.file.mkOutOfStoreSymlink ./backgrounds;
+
+      "hypr/hyprland.hardware.conf".text = config.modules.hyprland.hardwareConfig;
 
       "hypr/nix.conf".text = ''
         $defaultBrowser=${defaultBrowser}
