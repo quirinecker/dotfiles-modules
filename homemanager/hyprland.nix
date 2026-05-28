74e63c6c405d683f27b22032c8b4ecbce835e1a3
quirinecker <quirin.ecker@bajupa.com>
2026-05-28 18:51:25 +0200

    ported hyprland config to lua

--- homemanager/hyprland.nix
+++ homemanager/hyprland.nix
@@ -23,11 +23,14 @@ in
     ];
 
     xdg.configFile = {
-      "hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink ./hyprland/hyprland.conf;
+      "hypr/hyprland.lua".source = config.lib.file.mkOutOfStoreSymlink ./hyprland/hyprland.lua;
+
+      # The stups get linked to the config directory as well in order to use the autocomplete without copying it over in other places.
+      "hypr/hl.meta.lua".source = config.lib.file.mkOutOfStoreSymlink ./hyprland/hl.meta.lua;
       "hypr/hypridle.conf".source = config.lib.file.mkOutOfStoreSymlink ./hyprland/hypridle.conf;
       "backgrounds".source = config.lib.file.mkOutOfStoreSymlink ./backgrounds;
 
-      "hypr/hyprland.hardware.conf".text = config.modules.hyprland.hardwareConfig;
+      "hypr/hyprland_hardware.lua".text = config.modules.hyprland.hardwareConfig;
 
       "hypr/nix.conf".text = ''
         $defaultBrowser=${defaultBrowser}
