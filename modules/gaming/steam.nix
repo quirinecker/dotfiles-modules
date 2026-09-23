821a6d264806128bca140749cb332ebb79e754ed
quirinecker <quirin.ecker@bajupa.com>
2026-09-23 22:14:55 +0200

    gaming mode for steam now lives in its own modules

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -2,4 +2,17 @@
   flake.modules.nixos.steam = { ... }: {
     programs.steam.enable = true;
   };
+
+  # requires dotfiles-modules.modules.nixos.hyprland to function
+  # this is also only compatible with a home on the qepc system
+  flake.modules.homeManager.qepc-steamtv = { pkgs, ... }: {
+    dotfiles-modules.hyprland.extraConfig = ''
+      hl.bind(MainMod .. "+ SHIFT + G",
+        hl.dsp.exec_cmd(
+          "${pkgs.gamescope}/bin/gamescope -W 2560 -H 1440 -f -e -- steam -bigpicture",
+          { workspace = "9" }
+        )
+      )
+    '';
+  };
 }
